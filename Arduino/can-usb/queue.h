a37aec20656986f00d4ed1525ea6c5bd2dcef4ae
Dmitry <dmitry@pereslegin.ru>
2015-12-14 22:37:19 +0300

    Create queue.h

---
+++ Arduino/can-usb/queue.h
@@ -0,0 +1,16 @@
+#ifndef QUEUE_H_
+#define QUEUE_H_
+
+#define BUF_SIZE_RX 256 
+
+struct queue {
+    uint8_t buffer[BUF_SIZE_RX];
+    uint8_t in,
+    uint8_t out;
+};
+
+void writeToQueue(struct queue *q, uint8_t value);
+uint8_t readFromQueue(struct queue *q);
+uint8_t isQueueReady(struct queue *q);
+
+#endif /* QUEUE_H_ */
