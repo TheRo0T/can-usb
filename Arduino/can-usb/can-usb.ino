84a09ce46589b381feaecc6e59454f81c645e425
Dmitry <dmitry@pereslegin.ru>
2015-12-14 21:48:06 +0300

    Update can-usb.ino

--- Arduino/can-usb/can-usb.ino
+++ Arduino/can-usb/can-usb.ino
@@ -1,6 +1,6 @@
 #define BUF_SIZE_RX 256 
 
-#define USART_DataReady  (rxIn != rxOut)
+#define Queue_Ready(q)  ((q).in != (q).out)
 
 #define CMD_BUFFER_LEN 30  // Lenght command buffer
 #define ERR         7      // Error (ASCII BEL)
@@ -8,15 +8,17 @@
 #define LED_PIN 13
 
 //Буфер приема и его индексы:
-uint8_t bufRx [BUF_SIZE_RX];
-volatile uint8_t rxIn, rxOut;
+struct queue {
+    uint8_t buffer[BUF_SIZE_RX];
+    uint8_t in,
+    uint8_t out;
+} rxQueue;
 
 uint8_t cmdBuf[CMD_BUFFER_LEN];  // command buffer
 uint8_t bufIdx = 0;
 
-uint8_t readBuf() {
-  uint8_t tmpChar = bufRx[rxOut++];
-  return tmpChar;
+uint8_t readBuf(struct queue *q) {
+    return q->buffer[q->out++];
 }
 
 uint8_t execCmd(uint8_t * cmdBuf) {
@@ -37,16 +39,14 @@ uint8_t execCmd(uint8_t * cmdBuf) {
 }
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(LED_PIN, OUTPUT); 
+    Serial.begin(115200);
+    pinMode(LED_PIN, OUTPUT); 
 }
 
 void loop() {
   
-  uint8_t rxChar;
-  
-  if (USART_DataReady) {
-    rxChar = readBuf();
+  if (Queue_Ready(rxQueue)) {
+    uint8_t rxChar = readBuf(&rxQueue);
 //    Serial.write(rxChar);
     if (rxChar == '\r') {    // End command
       cmdBuf[bufIdx] = '\0'; // End string
@@ -60,5 +60,5 @@ void loop() {
 }
 
 void serialEvent() {
-  bufRx[rxIn++] = Serial.read();
+    rxQueue.buffer[rxQueue.in++] = Serial.read();
 }  
