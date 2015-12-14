1a44bbd6f14fe5428e5c30cd24922c6ee6f6be76
TheRo0T <dm-h@yandex.ru>
2015-12-14 22:30:47 +0400

    improved version

--- Arduino/can-usb/can-usb.ino
+++ Arduino/can-usb/can-usb.ino
@@ -1,8 +1,6 @@
-#include <avr/interrupt.h> //библиотека прерываний
-
 #define BUF_SIZE_RX 256 
-#define BUF_SIZE_TX 256 
-#define USART_DataReady  (rxIn != rxOut)    // Индексы буферов не равны, значит в буфере есть данные
+
+#define USART_DataReady  (rxIn != rxOut)
 
 #define CMD_BUFFER_LEN 30  // Длина буфера команды
 #define ERR         7      // Ошибка (ASCII BEL)
@@ -10,137 +8,57 @@
 #define LED_PIN 13
 
 //Буфер приема и его индексы:
-uint8_t bufRX [BUF_SIZE_RX];
+uint8_t bufRx [BUF_SIZE_RX];
 volatile uint8_t rxIn, rxOut;
 
-//Буфер передачи и его индексы:
-uint8_t bufTX [BUF_SIZE_TX];
-volatile uint8_t txIn, txOut;
-
-uint8_t cmdBuf[CMD_BUFFER_LEN];  // Буфер команды
+uint8_t cmdBuf[CMD_BUFFER_LEN];  // Буфер команд
 uint8_t bufIdx = 0;
 
-void initUSART (void) {
-   //Режим двойной скорости включен:
-   UCSR0A |= (1 << U2X0);
-   //Разрешены прерывание приема, работа приемника и передатчика:
-   UCSR0B = (1 << RXCIE0)|(1 << RXEN0)|(1 << TXEN0);
-   //Настройка размера фрейма данных 8 бит, без четности, 1 стоп-бит:
-   UCSR0C = (1 << UCSZ01)|(1 << UCSZ00);
-   //Настройка скорости 115200 bps:
-   UBRR0H = 0;
-   UBRR0L = 16;
-}
-
-
-uint8_t uartRead(void) {
-  uint8_t readValue = 0;
-    
-  UCSR0B &= ~(1 << RXCIE0);      // Запрещаем прерывание приёмника
-  readValue = bufRX[rxOut++];    // Считываем байт из буфера
-  UCSR0B |= (1 << RXCIE0);       // Разрешаем прерывание приёмника
-  
-  return readValue; 
-}
-
-
-void uartWrite(uint8_t txData) {
-  if((UCSR0B & (1 << TXCIE0)) == 0) {   // Отправляем сразу
-    UDR0 = txData;
-  }
-  else {                        
-    UCSR0B &= ~(1 << TXCIE0);   // Запрещаем прерывание передатчика
-    bufTX[txIn++] = txData;     // Сохраняем в буфер
-  }
-  UCSR0B |= (1 << TXCIE0);    // Разрешаем прерывание передатчика
-}
-
-ISR(USART_TX_vect) {
-   if(txIn != txOut) {
-      UDR0 = bufTX[txOut++];
-   } else {
-      UCSR0B &= ~(1 << TXCIE0);   // Запрещаем прерывание передатчика
-   }
-}
-
-
-ISR(USART_RX_vect) {
-   bufRX[rxIn++] = UDR0;
+uint8_t readBuf() {
+  uint8_t tmpChar = bufRx[rxOut++];
+  return tmpChar;
 }
 
 uint8_t execCmd(uint8_t * cmdBuf) {
   
   switch (cmdBuf[0]) {
     case 'V':
-      uartWrite('V');
-      uartWrite('0');
-      uartWrite('1');
-      uartWrite('0');
-      uartWrite('1');
+      Serial.print("V0101");
       return '\r';
     
     case 't':
-      uartWrite('t');
-      uartWrite('0');
-      uartWrite('3');
-      uartWrite('6');
-      uartWrite('5');
-      uartWrite('0');
-      uartWrite('1');
-      uartWrite('0');
-      uartWrite('2');
-      uartWrite('0');
-      uartWrite('3');
-      uartWrite('0');
-      uartWrite('4');
-      uartWrite('0');
-      uartWrite('5');
+      Serial.print("t03680102030405060708");
       return '\r';
-      
+    
     default:
       return ERR;
-    
   }
-    
-}
-
-uint8_t idxDiff (uint8_t idxIn, uint8_t idxOut)
-{
-    if (idxIn >= idxOut)
-        return (idxIn - idxOut);
-    else
-        return ((BUF_SIZE_RX - idxOut) + idxIn);
-}
-
-void setup() {
-  pinMode(LED_PIN, OUTPUT); 
-  initUSART();
-  sei();
   
 }
 
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT); 
+}
+
 void loop() {
-
+  
   uint8_t rxChar;
- 
+  
   if (USART_DataReady) {
-    rxChar = uartRead();
-//    uartWrite(rxChar);
-    
-    if (rxChar == '\r') {        // Символ конца команды
-      cmdBuf[bufIdx] = '\0';
-      uartWrite(execCmd(cmdBuf));
-
-      bufIdx = 0;                // Индекс буфера      
+    rxChar = readBuf();
+//    Serial.write(rxChar);
+    if (rxChar == '\r') {    // end command
+      cmdBuf[bufIdx] = '\0'; // end string
+      Serial.write(execCmd(cmdBuf));
+      bufIdx = 0; 
     }
-    
-    else if (rxChar != 0) {      // Сохраняем очередной символ в буфере
-      cmdBuf[bufIdx] = rxChar;
-      bufIdx++;   
-    }
-    
-    if (idxDiff(txIn, txOut) > 200){
-      digitalWrite(LED_PIN, HIGH);
-    }
+    else if (rxChar != 0) {
+      cmdBuf[bufIdx++] = rxChar;
+    } 
   }
 }
+
+void serialEvent() {
+  bufRx[rxIn++] = Serial.read();
+}  
